6fa85cfbb7b2e19d67aa790be6264f5100c35e13
Rusty Russell <rusty@rustcorp.com.au>
2014-01-06 19:20:54 +1030

    check_block: don't crash checking prev merkles if no batches at all.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- check_block.c
+++ check_block.c
@@ -260,7 +260,7 @@ bool check_block_prev_merkles(struct state *state,
 
 			/* We need to know everything in batch to check
 			 * previous merkle. */
-			if (!batch_full(prev, prev->batch[j]))
+			if (!prev->batch[j] || !batch_full(prev, prev->batch[j]))
 				continue;
 
 			/* Merkle has block reward address prepended, so you
