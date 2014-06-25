dfeacc2852262283198f712ba992d958ea13213d
Rusty Russell <rusty@rustcorp.com.au>
2014-06-25 13:58:23 +0930

    patch enhance-sizes.patch

--- sizes.c
+++ sizes.c
@@ -6,10 +6,27 @@
 
 #define AVERAGE_INPUTS 2.1
 
+static char *format_si(unsigned long long x)
+{
+	static char buf[100];
+	const char *suffix[] = { "", "Kilo", "Mega", "Giga", "Tera", NULL };
+	unsigned int i = 0;
+
+	while (x > 100 * 1024) {
+		if (!suffix[i+1])
+			break;
+		i++;
+		x /= 1024;
+	}
+
+	sprintf(buf, "%llu %sbytes", x, suffix[i]);
+	return buf;
+}
+
 int main(int argc, char *argv[])
 {
 	unsigned long long tps, tsize, trans_per_block, merkles_per_block,
-		blocksize, shard_order;
+		blocksize, shard_order, txbytes_per_sec, blockbytes_per_sec;
 
 	if (argc != 2)
 		errx(1, "Usage: sizes <tps>");
@@ -19,7 +36,7 @@ int main(int argc, char *argv[])
 		+ sizeof(struct protocol_input) * AVERAGE_INPUTS;
 	printf("# Assuming %f average inputs => tsize %llu bytes\n",
 	       AVERAGE_INPUTS, tsize);
-	trans_per_block = tps * 600;
+	trans_per_block = tps * BLOCK_TARGET_TIME;
 	printf("Transactions per block: %llu\n", trans_per_block);
 	/* Increase shard order until shards are half full. */
 	for (shard_order = PROTOCOL_INITIAL_SHARD_ORDER;
@@ -28,7 +45,7 @@ int main(int argc, char *argv[])
 
 	printf("Shards: (2^%llu) %llu\n", shard_order, 1ULL << shard_order);
 	if (shard_order > 16) {
-		printf("Truncating shards to 2^16 (need u32 shardnum!)\n");
+		printf("*** Truncating shards to 2^16 (need u32 shardnum!)\n");
 		shard_order = 16;
 	}
 
@@ -39,15 +56,37 @@ int main(int argc, char *argv[])
 		+ sizeof(struct protocol_block_tailer);
 	printf("Block size: %llu\n", blocksize);
 
+	txbytes_per_sec = tps * tsize;
+	blockbytes_per_sec = blocksize / BLOCK_TARGET_TIME;
+
+	printf("%-15s%15s%15s%15s %s\n",
+	       "", "Transactions", "Blocks", "Total", "Units");
+
 	/* Miners need every transaction + every block. */
-	printf("Miners: %llu + %llu = %llu bytes per second\n",
-	       tps * tsize, blocksize / 600,
-	       tps * tsize + blocksize / 600);
+	printf("%-15s%15llu%15llu%15llu %s\n",
+	       "Miners pipe",
+	       txbytes_per_sec, blockbytes_per_sec,
+	       txbytes_per_sec + blockbytes_per_sec,
+	       "bytes per second");
+	printf("%-15s%15llu%15llu%25s\n",
+	       "Miners storage",
+	       txbytes_per_sec * TRANSACTION_HORIZON_SECS,
+	       blockbytes_per_sec * TRANSACTION_HORIZON_SECS,
+	       format_si(txbytes_per_sec * TRANSACTION_HORIZON_SECS
+			 + blockbytes_per_sec * TRANSACTION_HORIZON_SECS));
 
 	/* Minimal node needs two shards + every block. */
-	printf("Minimal nodes: %llu + %llu = %llu bytes per second\n",
-	       ((tps * tsize) >> shard_order) * 2, blocksize / 600,
-	       ((tps * tsize) >> shard_order) * 2 + blocksize / 600);
-
+	txbytes_per_sec >>= shard_order - 1;
+	printf("%-15s%15llu%15llu%15llu %s\n",
+	       "Minimal pipe",
+	       txbytes_per_sec, blockbytes_per_sec,
+	       txbytes_per_sec + blockbytes_per_sec,
+	       "bytes per second");
+	printf("%-15s%15llu%15llu%25s\n",
+	       "Minimal storage",
+	       txbytes_per_sec * TRANSACTION_HORIZON_SECS,
+	       blockbytes_per_sec * TRANSACTION_HORIZON_SECS,
+	       format_si(txbytes_per_sec * TRANSACTION_HORIZON_SECS
+			 + blockbytes_per_sec * TRANSACTION_HORIZON_SECS));
 	return 0;
 }
