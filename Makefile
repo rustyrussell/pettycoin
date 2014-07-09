a170f12c00a3d53efd6fe9b0fae75f40f2cd71aa
Rusty Russell <rusty@rustcorp.com.au>
2014-07-09 16:10:39 +0930

    genesis: make it static.
    The genesis block itself doesn't need to meet any difficulty requirements,
    so make it deterministic.
    
    When we launch pettycoin, we'll use the bitcoin network to seed the genesis
    block: this usage is shown in the mkgenesis.c source.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 PETTYCOIN_OBJS := block.o check_block.o check_tx.o difficulty.o shadouble.o timestamp.o gateways.o hash_tx.o pettycoin.o merkle_txs.o merkle_recurse.o tx_cmp.o genesis.o marshal.o hash_block.o prev_txhashes.o state.o tal_packet.o dns.o netaddr.o peer.o peer_cache.o pseudorand.o welcome.o log.o generating.o blockfile.o pending.o log_helper.o txhash.o signature.o proof.o chain.o features.o todo.o base58.o sync.o create_refs.o shard.o packet_io.o tx.o complain.o block_shard.o recv_block.o input_refs.o peer_wants.o inputhash.o tx_in_hashes.o merkle_hashes.o
 GENERATE_OBJS := generate.o merkle_hashes.o merkle_recurse.o hash_tx.o tx_cmp.o shadouble.o marshal.o minimal_log.o timestamp.o tal_packet.o
-MKGENESIS_OBJS := mkgenesis.o shadouble.o marshal.o hash_block.o minimal_log.o
+MKGENESIS_OBJS := mkgenesis.o shadouble.o hash_block.o merkle_hashes.o merkle_recurse.o minimal_log.o
 SIZES_OBJS := sizes.o
 MKPRIV_OBJS := mkpriv.o
 INJECT_OBJS := inject.o base58.o create_tx.o marshal.o netaddr.o hash_tx.o minimal_log.o shadouble.o signature.o hash_block.o merkle_recurse.o
@@ -13,7 +13,9 @@ LDFLAGS = -O3 -flto
 LDLIBS := -lcrypto -lrt
 
 # We set this low for convenient testing.
-INITIAL_DIFFICULTY=0x1effffff
+TEST_GENESIS_DIFFICULTY=0x1effffff
+TEST_GENESIS_TIMESTAMP=1404886369
+TEST_GENESIS_NONCE=MarcusArabellAlex
 
 all: generate mkgenesis pettycoin sizes mkpriv inject
 
@@ -35,8 +37,8 @@ pettycoin: $(PETTYCOIN_OBJS) $(CCAN_OBJS)
 sizes: $(SIZES_OBJS) $(CCAN_OBJS)
 	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $(SIZES_OBJS) $(CCAN_OBJS) $(LDLIBS)
 
-genesis.c:
-	$(MAKE) mkgenesis generate && ./mkgenesis 4 $(INITIAL_DIFFICULTY) "Some NYT Head" > $@.tmp; STATUS=$$?; if [ $$STATUS = 0 ]; then mv $@.tmp $@; else rm -f $@.tmp; exit $$STATUS; fi
+genesis.c: mkgenesis
+	./mkgenesis $(TEST_GENESIS_DIFFICULTY) $(TEST_GENESIS_TIMESTAMP) $(TEST_GENESIS_NONCE) > $@.tmp; STATUS=$$?; if [ $$STATUS = 0 ]; then mv $@.tmp $@; else rm -f $@.tmp; exit $$STATUS; fi
 
 check: check-include-order
 	$(MAKE) -C test check
--- mkgenesis.c
+++ mkgenesis.c
@@ -1,144 +1,23 @@
-/* FIXME: Exclude genesis from difficulty check, so we can base on a
- * bitcoin block and be done. */
-/* This calculates the genesis block values.  Supply 8 bytes of nonce:
+/* This calculates the genesis block, eg, to base it off bitcoin:
  *
- * ./genesis 4 0x1dffffff "NYT Head"
+ * # Get 17 bytes of nonce. 
+ * NONCE=`wget https://blockchain.info/q/latesthash -q -O- | cut -c48-`
+ * # Use current date
+ * DATE=`date +%s`
+ *
+ * ./mkgenesis 0x1effffff $DATE "$NONCE" > genesis.c
  */
 #include "hash_block.h"
-#include "marshal.h"
+#include "merkle_hashes.h"
 #include "protocol.h"
 #include "shadouble.h"
+#include "shard.h"
+#include "version.h"
 #include <assert.h>
 #include <ccan/array_size/array_size.h>
 #include <ccan/err/err.h>
-#include <ccan/read_write_all/read_write_all.h>
-#include <ccan/str/str.h>
-#include <ccan/tal/tal.h>
-#include <signal.h>
 #include <stdio.h>
-#include <sys/select.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-
-struct worker {
-	int txs_to_worker;
-	int result_from_worker;
-	pid_t child;
-};
-
-static void destroy_worker(struct worker *w)
-{
-	kill(w->child, SIGTERM);
-	close(w->txs_to_worker);
-	close(w->result_from_worker);
-	waitpid(w->child, NULL, 0);
-}
-
-static const struct protocol_block_header *
-solve(const tal_t *ctx,
-      unsigned int threads,
-      const char *difficulty,
-      char *nonce,
-      const u8 **shard_nums,
-      const struct protocol_double_sha **merkles,
-      const struct protocol_block_tailer **tailer)
-{
-	unsigned int i, maxfd = 0;
-	fd_set set;
-	tal_t *children = tal(ctx, char);
-	struct protocol_pkt_block *ret;
-	const u8 *prev_merkles;
-	const struct protocol_block_header *hdr;
-	char shard_order[STR_MAX_CHARS(u8)];
-
-	sprintf(shard_order, "%u", PROTOCOL_INITIAL_SHARD_ORDER);
-
-	FD_ZERO(&set);
-
-	for (i = 0; i < threads; i++) {
-		struct worker *w = tal(children, struct worker);
-		int outfd[2], infd[2];
-
-		/* Set each child on a difference nonce. */
-		nonce[sizeof(hdr->nonce2)-1] += i % 96;
-		nonce[sizeof(hdr->nonce2)-2] += (i / 96) % 96;
-		assert(i < 96 * 96);
-
-		/* Create worker process. */
-		if (pipe(outfd) || pipe(infd)) {
-			tal_free(ctx);
-			err(1, "Creating pipes");
-		}
-
-		w->child = fork();
-		if (w->child == (pid_t)-1) {
-			tal_free(ctx);
-			err(1, "fork");
-		}
-		if (w->child == 0) {
-			close(outfd[0]);
-			close(infd[1]);
-			dup2(outfd[1], STDOUT_FILENO);
-			dup2(infd[0], STDIN_FILENO);
-			execl("./generate",
-			      "generate",
-			      /* Invalid reward address. */
-			      "0000000000000000000000000000000000000000",
-			      /* Difficult from cmdline. */
-			      difficulty,
-			      /* No previous block. */
-			      "00000000000000000000000000000000"
-			      "00000000000000000000000000000000",
-			      /* No prev merkles. */
-			      "0",
-			      /* Genesis block is 0 depth. */
-			      "0",
-			      shard_order,
-			      nonce, NULL);
-			exit(127);
-		}
-		close(outfd[1]);
-		close(infd[0]);
-		/* Write "go" byte. */
-		write(infd[1], "", 1);
-		w->txs_to_worker = infd[1];
-		w->result_from_worker = outfd[0];
-		tal_add_destructor(w, destroy_worker);
-		FD_SET(w->result_from_worker, &set);
-		if (w->result_from_worker > maxfd)
-			maxfd = w->result_from_worker;
-	}
-
-	select(maxfd+1, &set, NULL, NULL, NULL);
-	for (i = 0; i < maxfd+1; i++) {
-		if (FD_ISSET(i, &set)) {
-			struct protocol_pkt_block hdr;
-
-			if (read(i, &hdr, sizeof(hdr)) != sizeof(hdr)) {
-				tal_free(ctx);
-				err(1, "reading from child");
-			}
-			ret = (void *)tal_arr(ctx, char, le32_to_cpu(hdr.len));
-			*ret = hdr;
-			if (!read_all(i, ret + 1,
-				      le32_to_cpu(hdr.len) - sizeof(hdr))) {
-				tal_free(ctx);
-				err(1, "reading transaction from child");
-			}
-			break;
-		}
-	}
-
-	/* Kill off children. */
-	tal_free(children);
-
-	/* prev_merkles will be empty. */
-	unmarshal_block(NULL, ret,
-			&hdr, shard_nums, merkles, &prev_merkles, tailer);
-
-	return hdr;
-}
+#include <stdlib.h>
 
 static void dump_array(const u8 *arr, size_t len)
 {
@@ -152,75 +31,82 @@ static void dump_array(const u8 *arr, size_t len)
 
 int main(int argc, char *argv[])
 {
-	unsigned int threads;
-	tal_t *ctx = tal(NULL, char);
-	const struct protocol_block_header *hdr;
-	const struct protocol_block_tailer *tailer;
-	const struct protocol_double_sha *merkles;
+	struct protocol_block_header hdr;
+	struct protocol_block_tailer tailer;
+	struct protocol_double_sha merkles[1 << PROTOCOL_INITIAL_SHARD_ORDER];
 	struct protocol_double_sha sha;
-	const u8 *shard_nums;
+	u8 shard_nums[1 << PROTOCOL_INITIAL_SHARD_ORDER];
 	unsigned int i;
-	char nonce[sizeof(hdr->nonce2) + 1];
 
 	err_set_progname(argv[0]);
 
-	if (argc != 4)
-		errx(1, "Usage: ./genesis threads difficulty nonce");
+	if (argc != 4
+	    || strlen(argv[3]) != sizeof(hdr.nonce2) + sizeof(tailer.nonce1))
+		errx(1, "Usage: mkgenesis difficulty timestamp %zu-byte-nonce",
+			sizeof(hdr.nonce2) + sizeof(tailer.nonce1));
 
-	threads = strtol(argv[1], NULL, 0);
+	hdr.version = current_version();
+	hdr.features_vote = 0;
+	hdr.shard_order = PROTOCOL_INITIAL_SHARD_ORDER;
+	memcpy(hdr.nonce2, argv[3] + sizeof(tailer.nonce1), sizeof(hdr.nonce2));
+	memset(&hdr.prev_block, 0, sizeof(hdr.prev_block));
+	hdr.num_prev_txhashes = cpu_to_le32(0);
+	hdr.depth = cpu_to_le32(0);
+	memset(&hdr.fees_to, 0, sizeof(hdr.fees_to));
 
-	/* Copy nonce from commandline. */
-	memset(nonce, ' ', sizeof(nonce)-1);
-	if (strlen(argv[3]) > sizeof(hdr->nonce2))
-		errx(1, "Nonce cannot be more than %zu bytes",
-		     sizeof(hdr->nonce2));
-	memcpy(nonce, argv[3], strlen(argv[3]));
-	nonce[sizeof(nonce)-1] = '\0';
+	for (i = 0; i < num_shards(&hdr); i++)
+		shard_nums[i] = 0;
 
-	hdr = solve(ctx, threads, argv[2], nonce, &shard_nums, &merkles,
-		    &tailer);
+	for (i = 0; i < num_shards(&hdr); i++)
+		merkle_hashes(NULL, 0, 0, &merkles[i]);
 
+	tailer.timestamp = cpu_to_le32(strtoul(argv[2], NULL, 0));
+	tailer.difficulty = cpu_to_le32(strtoul(argv[1], NULL, 0));
+	memcpy(&tailer.nonce1, argv[3], sizeof(tailer.nonce1));
+
+	printf("/* Generated by: mkgenesis %s %s '%s' */\n",
+	       argv[1], argv[2], argv[3]);
 	printf("#include \"genesis.h\"\n");
 	printf("#include \"protocol.h\"\n\n");
 	printf("static struct protocol_block_header genesis_hdr = {\n");
-	printf("\t.version = %u,\n", hdr->version);
-	printf("\t.features_vote = %u,\n", hdr->features_vote);
-	printf("\t.shard_order = %u,\n", hdr->shard_order);
+	printf("\t.version = %u,\n", hdr.version);
+	printf("\t.features_vote = %u,\n", hdr.features_vote);
+	printf("\t.shard_order = %u,\n", hdr.shard_order);
 	{
 		printf("\t.nonce2 = ");
-		dump_array(hdr->nonce2, sizeof(hdr->nonce2));
+		dump_array(hdr.nonce2, sizeof(hdr.nonce2));
 		printf(",\n");
 	}
 	{
 		printf("\t.fees_to = { ");
-		dump_array(hdr->fees_to.addr, ARRAY_SIZE(hdr->fees_to.addr));
+		dump_array(hdr.fees_to.addr, ARRAY_SIZE(hdr.fees_to.addr));
 		printf(" }\n");
 	}
 	printf("};\n");
 
 	printf("static const struct protocol_block_tailer genesis_tlr = {\n");
 	printf("\t.timestamp = CPU_TO_LE32(%u),\n",
-	       le32_to_cpu(tailer->timestamp));
+	       le32_to_cpu(tailer.timestamp));
 	printf("\t.difficulty = CPU_TO_LE32(0x%08x),\n",
-	       le32_to_cpu(tailer->difficulty));
-	printf("\t.nonce1 = CPU_TO_LE32(%u)\n", le32_to_cpu(tailer->nonce1));
+	       le32_to_cpu(tailer.difficulty));
+	printf("\t.nonce1 = CPU_TO_LE32(%u)\n", le32_to_cpu(tailer.nonce1));
 	printf("};\n");
 
 	/* This will be all zeroes, but let's be precise. */
 	printf("static const u8 genesis_shardnums[] = {\n");
-	for (i = 0; i < (1U << hdr->shard_order); i++)
+	for (i = 0; i < num_shards(&hdr); i++)
 		printf("%s%u", i == 0 ? "" : ", ", shard_nums[i]);
 	printf("\n};\n");
 
 	printf("static const struct protocol_double_sha genesis_merkles[] = {\n");
-	for (i = 0; i < (1U << hdr->shard_order); i++) {
+	for (i = 0; i < num_shards(&hdr); i++) {
 		printf("{ ");
 		dump_array(merkles[i].sha, ARRAY_SIZE(merkles[i].sha));
 		printf("} ,\n");
 	}
 	printf("};\n");
 
-	for (i = 0; i < (1U << hdr->shard_order); i++) {
+	for (i = 0; i < num_shards(&hdr); i++) {
 		printf("static struct block_shard genesis_shard%i = {\n",
 		       i);
 		printf("\t.shardnum = %i\n", i);
@@ -228,12 +114,12 @@ int main(int argc, char *argv[])
 		printf("};\n");
 	}
 	printf("static struct block_shard *genesis_shards[] = {\n");
-	for (i = 0; i < (1U << hdr->shard_order); i++)
+	for (i = 0; i < num_shards(&hdr); i++)
 		printf("%s&genesis_shard%i",
 		       i == 0 ? "\t" : ", ", i);
 	printf("\n};\n");
 
-	hash_block(hdr, shard_nums, merkles, NULL, tailer, &sha);
+	hash_block(&hdr, shard_nums, merkles, NULL, &tailer, &sha);
 
 	printf("struct block genesis = {\n"
 	       "	.hdr = &genesis_hdr,\n"
@@ -241,11 +127,12 @@ int main(int argc, char *argv[])
 	       "	.merkles = genesis_merkles,\n"
 	       "	.tailer = &genesis_tlr,\n"
 	       "	.shard = genesis_shards,\n"
+	       "	.children = LIST_HEAD_INIT(genesis.children),\n"
+	       "	.all_known = true,\n"
 	       "	.sha = { ");
 
 	dump_array(sha.sha, ARRAY_SIZE(sha.sha));
 	printf("}\n};\n");
 
-	tal_free(ctx);
 	return 0;
 };
