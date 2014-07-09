f86a6a65d7627345b825b15b2037d840dadc204e
Rusty Russell <rusty@rustcorp.com.au>
2014-07-09 12:51:26 +0930

    Make sure we set unused field in welcome.
    Valgrind notices, too.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- welcome.c
+++ welcome.c
@@ -77,6 +77,7 @@ struct protocol_pkt_welcome *make_welcome(const tal_t *ctx,
 	w->random = state->random_welcome;
 	w->you = *a;
 	w->listen_port = state->listen_port;
+	w->unused = 0;
 	add_interests(state, &w, state->preferred_chain->hdr->shard_order);
 	add_welcome_blocks(state, &w);
 
