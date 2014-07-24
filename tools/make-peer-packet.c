098ef34dbb8daaa4123d085ec4f0e0c4526f1b09
Rusty Russell <rusty@rustcorp.com.au>
2014-07-24 16:59:24 +0930

    Remove serve_addresses (we can use inetd for now), rename mkaddr.
    Have it spit out all the addressess as a PROTOCOL_PKT_PEER we can simply
    serve.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- tools/make-peer-packet.c
+++ tools/make-peer-packet.c
@@ -1,4 +1,5 @@
 #include "../protocol_net.h"
+#include "../tal_packet.h"
 #include "../netaddr.h"
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -13,11 +14,15 @@
 int main(int argc, char *argv[])
 {
 	int i;
+	struct protocol_pkt_peers *pkt;
 
 	err_set_progname(argv[0]);
 
 	if (argc == 1 || argc % 2 != 1)
 		errx(1, "Usage: %s <addr> <port> [<addr> <port>...]", argv[0]);
+
+	pkt = tal_packet(NULL, struct protocol_pkt_peers,
+			 PROTOCOL_PKT_PEERS);
 
 	for (i = 1; i < argc; i += 2) {
 		struct addrinfo *a = net_client_lookup(argv[i], argv[i+1],
@@ -28,21 +33,22 @@ int main(int argc, char *argv[])
 
 		while (a) {
 			struct protocol_net_address netaddr;
+
 			if (!addrinfo_to_netaddr(&netaddr, a))
 				err(1, "Invalid address");
 
 			/* As long as they're distinct, it's OK. */
 			memcpy(&netaddr.uuid, &i, sizeof(i));
 
-			fprintf(stderr, "Writing out %s address for %s\n",
+			fprintf(stderr, "Got %s address for %s\n",
 				a->ai_family == AF_INET ? "IPv4" : "IPv6",
 				argv[i]);
-
-			if (!write_all(STDOUT_FILENO, &netaddr,
-				       sizeof(netaddr)))
-				err(1, "Writing out netaddr");
+			tal_packet_append(&pkt, &netaddr, sizeof(netaddr));
 			a = a->ai_next;
 		}
 	}
+
+	if (!write_all(STDOUT_FILENO, pkt, le32_to_cpu(pkt->len)))
+		err(1, "Writing out packet");
 	return 0;
 }
