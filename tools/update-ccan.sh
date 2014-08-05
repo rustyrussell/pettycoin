a04c8105c0ecd94f5c34e5bb2c8cd1c4539bee03
Rusty Russell <rusty@rustcorp.com.au>
2014-08-05 10:57:45 +0930

    tools/update-ccan.sh: Fixes, and make it a bit friendlier.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- tools/update-ccan.sh
+++ tools/update-ccan.sh
@@ -3,17 +3,25 @@
 set -e
 
 CCANDIR=${1:-../ccan}
-NEW_VERSION=$(git --git-dir=$CCANDIR/.git describe --always)
+NEW_VERSION=${2:-$(git --git-dir=$CCANDIR/.git describe --always)}
 OLD_VERSION=$(grep '^CCAN version: ' ccan/README | cut -d: -f2)
 
+if [ $NEW_VERSION = $OLD_VERSION ]; then
+    echo Already at $OLD_VERSION
+    exit 0
+fi
+
 # Make sure we have a clean tree.
-if git status --porcelain | grep -v '^\?\?'; then
+UNCLEAN=$(git status --porcelain | grep -v '^??' || true)
+if [ -n "$UNCLEAN" ]; then
     echo "Dirty tree" >&2
     exit 1
 fi
 
-(cd $CCANDIR && git diff $OLD_VERSION $NEW_VERSION ccan/ licenses/ tools/configurator) > ccan/diff
+MODULES=$(cd ccan; echo ccan/*)
+(cd $CCANDIR && git diff $OLD_VERSION $NEW_VERSION $MODULES licenses/ tools/configurator) > ccan/diff
 (cd ccan && patch -p1 < diff)
+diffstat ccan/diff
 rm ccan/diff
 grep -v '^CCAN version: ' ccan/README > ccan/README.new
 echo "CCAN version: $NEW_VERSION" >> ccan/README.new
