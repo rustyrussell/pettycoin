29759246cca2b14d6cb31978f37fc4b353befaf1
Rusty Russell <rusty@rustcorp.com.au>
2014-08-06 13:02:10 +0930

    Use submitblock for blackbox test.
    Instead of waiting for generator.  Runs in about 23 seconds here now.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- test/blackbox/test-01-sync.sh
+++ test/blackbox/test-01-sync.sh
@@ -5,6 +5,8 @@ set -em
 rm -rf test-01-dir
 mkdir test-01-dir test-01-dir/p1 test-01-dir/p2
 
+NEWBLOCK=100100000800000000000000010002dcf2b3ff756f4c39c16a417e69138d79ce03488dda58e4f53cf640b3a99b91de4e8561afa441badda92600000028000000521f00001201212a5b494784a924f2e0249bf4c268af1d0e000000006d07571dee2e35e1378bc43a8e1388d2fef6b3021cc9924b885d53b2ce390ea86d07571dee2e35e1378bc43a8e1388d2fef6b3021cc9924b885d53b2ce390ea86d07571dee2e35e1378bc43a8e1388d2fef6b3021cc9924b885d53b2ce390ea86d07571dee2e35e1378bc43a8e1388d2fef6b3021cc9924b885d53b2ce390ea83e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e3e449ee153c4ff3b1d7a7f0901
+
 # Unpack their blockfiles.
 xzcat test-01-blockfile1.xz > test-01-dir/p1/blockfile
 xzcat test-01-blockfile2.xz > test-01-dir/p2/blockfile
@@ -17,14 +19,11 @@ ln -sf ../addresses test-01-dir/p2/addresses
 # Core dumps please!
 ulimit -c unlimited
 
-# P1 generates
-../../pettycoin --seeding --developer-test --pettycoin-dir=test-01-dir/p1 --generator=../../../../pettycoin-generate --reward-address=P-mhA9ozMTVWrSnUX2kB8QjEq9FBen8k3euW  > test-01-dir/p1.log 2>&1 &
-
-# P2 syncs
+../../pettycoin --seeding --developer-test --pettycoin-dir=test-01-dir/p1 > test-01-dir/p1.log 2>&1 &
 ../../pettycoin --seeding --developer-test --pettycoin-dir=test-01-dir/p2 > test-01-dir/p2.log 2>&1 &
 
-# They should sync within 5 minutes.
-END=$(( $(date +%s) + 300 ))
+# They should complete within 1 minute.
+END=$(( $(date +%s) + 60 ))
 
 # Wait for startup
 while [ $(date +%s) -lt $END ]; do
@@ -33,6 +32,9 @@ while [ $(date +%s) -lt $END ]; do
     fi
     sleep 5
 done
+
+# Give P1 new block, will trigger P2 to ask.
+../../pettycoin-query --pettycoin-dir=test-01-dir/p1 submitblock $NEWBLOCK
 
 while [ $(date +%s) -lt $END ]; do
     DEPTH1=`../../pettycoin-query --pettycoin-dir=test-01-dir/p1 getinfo | sed -n 's/.*"height" : \([0-9]\+\) .*num_todos.*/\1/p'`
