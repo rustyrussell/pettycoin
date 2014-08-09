ba0fe87adc871427377fcd0147a80124543c1822
Rusty Russell <rusty@rustcorp.com.au>
2014-08-09 10:00:45 +0930

    peer: use recv_tx_from_peer().
    We made the code more general and moved it into recv_tx.c, but never called
    it!  Fix that so we don't have duplicated code (they're currently identical).
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- peer.c
+++ peer.c
@@ -22,6 +22,7 @@
 #include "protocol_net.h"
 #include "recv_block.h"
 #include "recv_complain.h"
+#include "recv_tx.h"
 #include "shadouble.h"
 #include "shard.h"
 #include "state.h"
@@ -964,124 +965,6 @@ recv_get_tx(struct peer *peer,
 }
 
 static enum protocol_ecode
-recv_tx_in_block(struct peer *peer, const struct protocol_pkt_tx_in_block *pkt)
-{
-	enum protocol_ecode e;
-	union protocol_tx *tx;
-	struct protocol_input_ref *refs;
-	struct protocol_tx_with_proof *proof;
-	struct block *b;
-	struct protocol_double_sha sha;
-	u16 shard;
-	u8 conflict_txoff;
-	size_t len = le32_to_cpu(pkt->len), used;
-
-	if (len < sizeof(*pkt))
-		return PROTOCOL_ECODE_INVALID_LEN;
-
-	len -= sizeof(*pkt);
-
-	e = le32_to_cpu(pkt->err);
-	if (e) {
-		struct protocol_position *pos = (void *)(pkt + 1);
-
-		if (len != sizeof(*pos))
-			return PROTOCOL_ECODE_INVALID_LEN;
-
-		if (e == PROTOCOL_ECODE_UNKNOWN_BLOCK) {
-			/* They don't know block at all, so don't ask. */
-			todo_done_get_block(peer, &pos->block, false);
-		} else if (e != PROTOCOL_ECODE_UNKNOWN_TX)
-			return PROTOCOL_ECODE_UNKNOWN_ERRCODE;
-
-		todo_done_get_tx_in_block(peer, &pos->block,
-					  le16_to_cpu(pos->shard),
-					  pos->txoff, false);
-		return PROTOCOL_ECODE_NONE;
-	}
-
-	if (len < sizeof(*proof))
-		return PROTOCOL_ECODE_INVALID_LEN;
-	len -= sizeof(*proof);
-
-	proof = (void *)(pkt + 1);
-	shard = le16_to_cpu(proof->proof.pos.shard);
-
-	b = block_find_any(peer->state, &proof->proof.pos.block);
-	if (!b) {
-		todo_add_get_block(peer->state, &proof->proof.pos.block);
-		/* FIXME: should we extract transaction? */
-		return PROTOCOL_ECODE_NONE;
-	}
-
-	/* FIXME: We could check the proof before we check the tx & refs,
-	 * then if a buggy implementation tried to send us invalid tx
-	 * and refs we could turn it into a complaint. */
-
-	tx = (void *)(proof + 1);
-	e = unmarshal_tx(tx, len, &used);
-	if (e)
-		return e;
-	len -= used;
-
-	/* You can't send us bad txs this way: use a complaint packet. */
-	e = check_tx(peer->state, tx, b);
-	if (e)
-		return e;
-
-	refs = (void *)((char *)tx + used);
-	e = unmarshal_input_refs(refs, len, tx, &used);
-	if (e)
-		return e;
-
-	if (used != len)
-		return PROTOCOL_ECODE_INVALID_LEN;
-
-	e = check_refs(peer->state, b, refs, num_inputs(tx));
-	if (e)
-		return e;
-
-	if (!check_proof(&proof->proof, b, tx, refs))
-		return PROTOCOL_ECODE_BAD_PROOF;
-
-	/* Whatever happens from here, no point asking others for tx. */
-	todo_done_get_tx_in_block(peer, &proof->proof.pos.block,
-				  shard, proof->proof.pos.txoff, true);
-
-	/* This may have been a response to GET_TX as well. */
-	hash_tx(tx, &sha);
-	todo_done_get_tx(peer, &sha, true);
-
-	/* Now it's proven that it's in the block, handle bad inputs/refs.
-	 * We don't hang up on them, since they may not have known. */
-	if (!check_tx_inputs_and_refs(peer->state, b, &proof->proof, tx, refs))
-		return PROTOCOL_ECODE_NONE;
-
-	/* Simularly, they might not know if it was misordered. */
-	if (!check_tx_ordering(peer->state, b, b->shard[shard],
-			       proof->proof.pos.txoff, tx, &conflict_txoff)) {
-		/* Tell everyone that txs are out of order in block */
-		complain_misorder(peer->state, b, &proof->proof,
-				  tx, refs, conflict_txoff);
-		return PROTOCOL_ECODE_NONE;
-	}
-
-	/* Keep proof in case anyone asks. */
-	put_proof_in_shard(peer->state, b, &proof->proof);
-	/* Copy in tx and refs. */
-	put_tx_in_shard(peer->state, peer,
-			b, b->shard[shard], proof->proof.pos.txoff,
-			txptr_with_ref(b->shard[shard], tx, refs));
-
-	/* This is OK for now, will be spammy in real network! */
-	log_info(peer->log, "gave us TX in shard %u, off %u, block %u ",
-		 shard, proof->proof.pos.txoff, le32_to_cpu(b->hdr->height));
-	log_add_struct(peer->log, struct protocol_double_sha, &sha);
-
-	return PROTOCOL_ECODE_NONE;
-}
-
-static enum protocol_ecode
 recv_get_txmap(struct peer *peer, const struct protocol_pkt_get_txmap *pkt,
 	       void **reply)
 {
@@ -1473,7 +1356,7 @@ static struct io_plan *pkt_in(struct io_conn *conn, struct peer *peer)
 		err = recv_get_tx_in_block(peer, peer->incoming, &reply);
 		break;
 	case PROTOCOL_PKT_TX_IN_BLOCK:
-		err = recv_tx_in_block(peer, peer->incoming);
+		err = recv_tx_from_peer(peer, peer->incoming);
 		break;
 	case PROTOCOL_PKT_GET_TX:
 		err = recv_get_tx(peer, peer->incoming, &reply);
