rustyrussell/pettycoin

commit d0b20ca652f7e3daeee3f74d768c1a1e94d4077f

protocol: fixed 8k interest bitmap (BREAKS PROTOCOL)
Might as well spell it out; it's only 8k.

Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

peer_wants.c
@@ -9,13 +9,12 @@ static bool peer_wants_shard(const struct peer *peer, u16 shard)
 	const u8 *interests = (const u8 *)(peer->welcome + 1);
 
 	return interests[shard/8] & (1 << (shard % 8));
-}	
+}
 
-/* Is this tx in the shard of this tx? */
+/* Is this tx in a shard wanted by this peer? */
 bool peer_wants_tx(const struct peer *peer, const union protocol_tx *tx)
 {
-	return peer_wants_shard(peer,
-				shard_of_tx(tx, peer->welcome->shard_order));
+	return peer_wants_shard(peer, shard_of_tx(tx, 16));
 }
 
 /* Is this tx in the (other) shard affected by this tx? */
@@ -29,8 +28,7 @@ bool peer_wants_tx_other(const struct peer *peer, const union protocol_tx *tx)
 		return false;
 	case TX_NORMAL:
 		/* This also affects shard of output address. */
-		shard = shard_of(&tx->normal.output_addr,
-				 peer->welcome->shard_order);
+		shard = shard_of(&tx->normal.output_addr, 16);
 		return peer_wants_shard(peer, shard);
 	case TX_TO_GATEWAY:
 	case TX_CLAIM:

protocol_net.h
@@ -114,13 +114,9 @@ struct protocol_pkt_welcome {
 	le16 listen_port;
 	/* How many block hashes at end. */
 	le16 num_blocks;
-	/* Our shard_order. */
-	u8 shard_order;
-	/* Pad to 32 bits */
-	u8 unused[3];
+	/* What shards we're interested in. */
+	u8 interests[65536/8];
 	/* Followed by:
-	   What addresses we're interested in (based on lower bits)
-	     u8 interests[(num_shards + 31) / 32 * 4];
 	   Blocks we know about: 10, then power of 2 back.
 	     struct protocol_block_id block[num_blocks];
 	*/

state.c
@@ -54,7 +54,7 @@ struct state *new_state(bool test_net)
 	s->log = new_log(s, NULL, "", s->log_level, STATE_LOG_MAX);
 	s->generator = "pettycoin-generate";
 	s->reward_addr = NULL;
-	memset(s->interests, 0xff, sizeof(s->interests)); /* Everything */
+	bitmap_fill(s->interests, 65536); /* Everything */
 	s->require_non_gateway_tx_fee = false;
 	s->require_gateway_tx_fee = false;
 

state.h
@@ -98,7 +98,7 @@ struct state {
 	int blockfd;
 
 	/* Which shards are we interested in. */
-	BITMAP_DECLARE(interests, 1 << PROTOCOL_INITIAL_SHARD_ORDER);
+	BITMAP_DECLARE(interests, 65536);
 };
 
 struct state *new_state(bool test_net);

welcome.c
@@ -48,22 +48,6 @@ out:
 	(*w)->num_blocks = cpu_to_le16(n);
 }
 
-static void add_interests(const struct state *state,
-			  struct protocol_pkt_welcome **w,
-			  u8 shard_order)
-{
-	size_t i, maplen = ((1 << shard_order) + 31) / 32 * 4;
-	u8 *map = tal_arrz(*w, u8, maplen);
-
-	for (i = 0; i < (1 << shard_order); i++)
-		if (interested_in_shard(state, i, shard_order))
-			map[i / 8] |= (1 << (i % 8));
-
-	(*w)->shard_order = shard_order;
-	tal_packet_append(w, map, maplen);
-	tal_free(map);
-}
-
 struct protocol_pkt_welcome *make_welcome(const tal_t *ctx,
 					  const struct state *state,
 					  const struct protocol_net_address *a)
@@ -77,8 +61,7 @@ struct protocol_pkt_welcome *make_welcome(const tal_t *ctx,
 	w->uuid = state->uuid;
 	w->you = *a;
 	w->listen_port = cpu_to_le16(state->listen_port);
-	memset(w->unused, 0, sizeof(w->unused));
-	add_interests(state, &w, state->preferred_chain->hdr->shard_order);
+	memcpy(w->interests, state->interests, sizeof(w->interests));
 	add_welcome_blocks(state, &w);
 
 	return w;
@@ -98,8 +81,7 @@ enum protocol_ecode check_welcome(const struct state *state,
 				  const struct protocol_pkt_welcome *w,
 				  const struct protocol_block_id **blocks)
 {
-	size_t len = le32_to_cpu(w->len), interest_len;
-	const u8 *interest;
+	size_t len = le32_to_cpu(w->len);
 	const struct block *genesis = genesis_block(state);
 
 	if (len < sizeof(*w))
@@ -108,28 +90,14 @@ enum protocol_ecode check_welcome(const struct state *state,
 		return PROTOCOL_ECODE_UNKNOWN_COMMAND;
 	if (w->version != cpu_to_le32(current_version()))
 		return PROTOCOL_ECODE_HIGH_VERSION;
+	/* This is too lenient, but future-proof. */
+	if (popcount(w->interests, 65536) < 2)
+		return PROTOCOL_ECODE_NO_INTEREST;
 
 	len -= sizeof(*w);
 
-	/* Check number of shards and shard interest bitmap */
-	if (w->shard_order < PROTOCOL_INITIAL_SHARD_ORDER)
-		return PROTOCOL_ECODE_BAD_SHARD_ORDER;
-	/* Must be reasonable. */
-	if (w->shard_order > PROTOCOL_MAX_SHARD_ORDER)
-		return PROTOCOL_ECODE_BAD_SHARD_ORDER;
-
-	/* Interest map follows base welcome struct. */
-	interest = (u8 *)(w + 1);
-	interest_len = (((size_t)1 << w->shard_order) + 31) / 32 * 4;
-	if (len < interest_len)
-		return PROTOCOL_ECODE_INVALID_LEN;
-	if (popcount(interest, (size_t)1 << w->shard_order) < 2)
-		return PROTOCOL_ECODE_NO_INTEREST;
-
-	len -= interest_len;
-
-	/* Blocks follow interest map. */
-	(*blocks) = (struct protocol_block_id *)(interest + interest_len);
+	/* Blocks follow header. */
+	(*blocks) = (struct protocol_block_id *)(w + 1);
 
 	/* At least one block. */
 	if (le16_to_cpu(w->num_blocks) < 1)

commit f139b3388ea5c53dc1a1a3e54c9f3bea9896635c

peer_wants: fix silly bug in detecting peer's interests.
This was caused by d0b20ca652f7e3daeee3f74d768c1a1e94d4077f, which
put the interest map into the welcome packet, but didn't fix this code.

Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

peer_wants.c
@@ -6,7 +6,7 @@
 
 static bool peer_wants_shard(const struct peer *peer, u16 shard)
 {
-	const u8 *interests = (const u8 *)(peer->welcome + 1);
+	const u8 *interests = peer->welcome->interests;
 
 	return interests[shard/8] & (1 << (shard % 8));
 }